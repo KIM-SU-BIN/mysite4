0392020bc338d3b817c5e5ad910c4cefdd31935b
subintoto <subintoto@naver.com>
2022-06-19 00:59:50 +0900

    addList 추가

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -1,5 +1,30 @@
 package com.javaex.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class GuestbookController {
 
+	// 필드
+
+	// 생성자
+
+	// 메소드
+
+	// 메소드 일반
+	//삭제폼 deleteForm
+	
+	
+	
+	
+	
+	//방명록 addList
+	@RequestMapping(value="/addList", method = {RequestMethod.GET,RequestMethod.POST})
+	public String addList() {
+		System.out.println("GuestController>addList");
+		
+		return "guestbook/addList";
+	}
 }
