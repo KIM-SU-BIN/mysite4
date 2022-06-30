0fc10eb924c4e875580bf46570f64f25d242c370
subintoto <subintoto@naver.com>
2022-06-30 18:47:24 +0900

    file 저장

---
+++ src/main/java/com/javaex/dao/FileDao.java
@@ -0,0 +1,30 @@
+package com.javaex.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.FileVo;
+
+@Repository
+public class FileDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	// 생성자
+
+	// 메소드
+
+	// 메소드 일반
+	public int insertFile(FileVo fileVo) {
+		System.out.println("FileDao>insertFile");
+		
+		int count = sqlSession.insert("insert.file", fileVo);
+
+		return count;
+		//return sqlSession.insert("insert.file", fileVo);
+
+	}
+
+}
